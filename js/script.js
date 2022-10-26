9a7833b099a362fda792ea0f7da51216dec673b3
Fintisov Vadim <fintisov.vadim@gmail.com>
2022-10-26 17:53:42 +0300

    added script for "notification"

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const header = document.querySelector(".header");
+
+    header.addEventListener("click", (e) => {
+        const target = e.target;
+
+        if (target.closest(".notification-close") && target.closest(".header__notification")) {
+            target.closest(".header__notification").classList.add("hidden")
+        }
+
+
+    })
+})
