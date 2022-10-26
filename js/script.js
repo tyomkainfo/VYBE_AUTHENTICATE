08ddeefb69800a0cc40bb6364439eb2b64595f0a
tyomka.info <tyomka.info@gmail.com>
2022-10-26 16:58:56 +0300

    done centrall part

--- js/script.js
+++ js/script.js
@@ -11,3 +11,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
     })
 })
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    navText: [ '', ' ' ],
+
+    responsive:{
+        0:{
+            items:1
+        },
+
+        1000:{
+            items:5
+        }
+    }
+});
